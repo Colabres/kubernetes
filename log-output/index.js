6820e7ad549145c37be0af40b142ade7b85e5c3b
Colabres <146994007+Colabres@users.noreply.github.com>
2026-07-13 10:39:35 +0300

    Exersise 1.7

--- log-output/index.js
+++ log-output/index.js
@@ -1,11 +1,26 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 const id = Math.random().toString(36).substr(2);
 
+const getStatus = () => {
+    return `${new Date().toISOString()}: ${id}`;
+};
+
 const printLog = () => {
-    
-    console.log(`${new Date().toISOString()}: ${id}`);
-
+    console.log(getStatus());
     setTimeout(printLog, 5000);
-}
+};
 
-printLog();     
+app.get("/", (req, res) => {
+    res.send(getStatus());
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
+
+printLog();
