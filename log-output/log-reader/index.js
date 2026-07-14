648e01d9341c4cd668306e3cca17c75b16c57fb0
Colabres <146994007+Colabres@users.noreply.github.com>
2026-07-14 13:47:56 +0300

    Exersise 2.1

--- log-output/log-reader/index.js
+++ log-output/log-reader/index.js
@@ -8,21 +8,27 @@ const PORT = process.env.PORT || 3000;
 
 const filePath = "/usr/src/app/files/log.txt";
 const counterFile = '/usr/src/app/files/counter.txt';
+const id = Math.random().toString(36).substr(2); //remove after task 2.1
 
-const getStatus = () => {
-    const log = fs.existsSync(filePath)
-    ? fs.readFileSync(filePath,"utf8")
-    :  "No log yet" ;
+const getStatus = async () => {
+    // const log = fs.existsSync(filePath)
+    // ? fs.readFileSync(filePath,"utf8")
+    // :  "No log yet" ;
 
-    const counter = fs.existsSync(counterFile)
-    ? fs.readFileSync(counterFile,"utf8")
-    : "0"
+    // const counter = fs.existsSync(counterFile)
+    // ? fs.readFileSync(counterFile,"utf8")
+    // : "0"
+    const response = await fetch("http://ping-pong-svc:2345/pings");
+    const counter = await response.text();
+    const log = `${new Date().toISOString()}: ${id}`;
+    
 
     return `${log}\nPing / Pongs: ${counter}`;
 };
 
-app.get("/", (req, res) => {
-    res.send(getStatus());
+app.get("/", async (req, res) => {
+    const status = await getStatus();
+    res.type("text/plain").send(status);
 });
 
 app.listen(PORT, () => {
