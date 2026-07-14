32d62b5054ca3f5d56d1d536ae746c6ec1f5e28d
Colabres <146994007+Colabres@users.noreply.github.com>
2026-07-14 22:50:40 +0300

    Exersise 2.5

--- log-output/log-reader/index.js
+++ log-output/log-reader/index.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 3000;
 
 const filePath = "/usr/src/app/files/log.txt";
 const counterFile = '/usr/src/app/files/counter.txt';
+const infoFilePath ="/usr/src/app/config/information.txt"
+const message = process.env.message
 const id = Math.random().toString(36).substr(2); //remove after task 2.1
 
 const getStatus = async () => {
@@ -18,12 +20,21 @@ const getStatus = async () => {
     // const counter = fs.existsSync(counterFile)
     // ? fs.readFileSync(counterFile,"utf8")
     // : "0"
+
+    const content = fs.existsSync(infoFilePath)
+    ? fs.readFileSync(infoFilePath,"utf8")
+    :  "No text yet" ;
+
     const response = await fetch("http://ping-pong-svc:2345/pings");
     const counter = await response.text();
+    
     const log = `${new Date().toISOString()}: ${id}`;
     
 
-    return `${log}\nPing / Pongs: ${counter}`;
+    return `file content: ${content}
+    env variable: MESSAGE=${message}
+    ${log}.
+    Ping / Pongs: ${counter}`;
 };
 
 app.get("/", async (req, res) => {
